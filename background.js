d83def9eaaff946a8762d3896b45795ef1cc0ccf
Imran Bepari <imranbepari97@gmail.com>
2019-09-21 14:59:36 +0100

    updated extension to have icons

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
 const oldReddit = "https://old.reddit.com";
 
+//Literally a basic function to navigate to a URL in the current tab 
 function navigate(url) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.update(tabs[0].id, {url: url});
     });
 }
 
+
 chrome.omnibox.onInputEntered.addListener(
 	function(text) {
-		//alert(text);
 		navigate(oldReddit + "/r/" + text);
 	}
 );
